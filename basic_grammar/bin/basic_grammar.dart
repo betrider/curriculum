1009b562622f354f1cfa93563f31a6d55c58f8c2
betrider <tlsfpem@gmail.com>
2023-09-12 13:42:12 +0900

    typedef추가

--- basic_grammar/bin/basic_grammar.dart
+++ basic_grammar/bin/basic_grammar.dart
@@ -1,10 +1,34 @@
-// ignore_for_file: unused_local_variable
+// ignore_for_file: unused_local_variable, dead_code, unnecessary_type_check
 
 void main(List<String> arguments) {
   print('기초 문법 시작');
 
-  //? variables → ---------------------------------------------------------------------- ↓ ↓
+  // 1.variables(변수)
+  variablesFunction();
 
+  // 2.casting(캐스팅)
+  castingFunction();
+
+  // 3.operators(연산자)
+  operatorsFunction();
+
+  // 4.comments(주석)
+  commentsFunction();
+
+  // 5.metadata(메타데이터)
+  metadataFunction();
+
+  // 6.library(라이브러리)
+  libraryFunction();
+
+  // 7.keywords(키워드)
+  keywordsFunction();
+
+  print('기초 문법 종료');
+}
+
+/// 1.변수
+void variablesFunction() {
   dynamic dynamicValue = 'dart'; // 가변타입(null o)
   dynamicValue = 55;
 
@@ -24,9 +48,10 @@ void main(List<String> arguments) {
 
   const int constValue = 10000; // 상수(컴파일 타임)
   final DateTime finalValue = DateTime.now(); // 상수(런타임 타임)
+}
 
-  //? casting → ---------------------------------------------------------------------- ↓ ↓
-
+/// 2.캐스팅
+void castingFunction() {
   //* 업캐스팅(불필요) 자식클래스 -> 부모클래스
   //* 다운캐스팅 부모클래스 -> 자식클래스
 
@@ -50,20 +75,20 @@ void main(List<String> arguments) {
   // -> num
   numValue2 = 15;
   numValue2 = 7.5;
-  numValue2 = int.parse(stringValue2) ;
-  numValue2 = double.parse(stringValue2) ;
+  numValue2 = int.parse(stringValue2);
+  numValue2 = double.parse(stringValue2);
   numValue2 = intValue2; // 업캐스팅 불필요
   numValue2 = doubleValue2; // 업캐스팅 불필요
 
   // -> int
   intValue2 = 15;
-  intValue2 = int.parse(stringValue2) ;
+  intValue2 = int.parse(stringValue2);
   intValue2 = numValue2.toInt();
   intValue2 = doubleValue2.toInt();
 
   // -> double
   doubleValue2 = 15;
-  doubleValue2 = double.parse(stringValue2) ;
+  doubleValue2 = double.parse(stringValue2);
   doubleValue2 = intValue2.toDouble();
   doubleValue2 = numValue2.toDouble(); // numValue2 as double(다운캐스팅)
 
@@ -72,21 +97,138 @@ void main(List<String> arguments) {
   boolValue2 = bool.parse('false');
 
   // -> DateTime
-  dateTimeValue2 = DateTime.parse('1974-03-20 00:00:00.000');
-  print('year:${dateTimeValue2.year} month:${dateTimeValue2.month} day:${dateTimeValue2.day} hour:${dateTimeValue2.hour} minute:${dateTimeValue2.minute}');
+  dateTimeValue2 = DateTime.parse('1992-03-20 00:00:00.000');
 
   // -> enum
-  enumValue2 = DayOfWeek.strToEnum('monday');
+  enumValue2 = DayOfWeek.strToEnum('monday'); // DayOfWeek.monday
+}
 
-  //? operators → --------------------------------------------------------------------- ↓ ↓
+/// 3.연산자
+void operatorsFunction() {
+  // 산술 연산자
+  print(5 + 2); // 7
+  print(5 - 2); // 3
+  print(5 / 2); // 2.5
+  print(5 ~/ 2); // 2
+  print(5 % 2); // 1
 
-  //? comments → ---------------------------------------------------------------------- ↓ ↓
+  // 항등 및 관계 연산자
+  print(2 == 2); // true
+  print(2 != 3); // true
+  print(3 > 2); // true
+  print(2 < 3); // true
+  print(3 >= 3); // true
+  print(2 <= 3); // true
 
-  //? metadata → ---------------------------------------------------------------------- ↓ ↓
+  // 유형 테스트 연산자
+  print(5.5 is int);
+  print(5.5 is! int);
+  print(5.5 is double);
+  print(5.5 is num);
+  print(5.5 is Object);
 
-  //? keywords → ---------------------------------------------------------------------- ↓ ↓
+  // 할당 연산자(생략)
 
-  print('기초 문법 종료');
+  // 논리 연산자
+  print(false && false); // false
+  print(true && false); // false
+  print(false && true); // false
+  print(true && true); // true
+
+  // 비트 및 시프트 연산자(생략)
+
+  // 삼항 연산자
+  bool boolValue3 = (1 + 1 == 5) ? true : false;
+  print(boolValue3);
+
+  // 캐스케이드
+
+  // 일반적인 방법
+  DateTime now = DateTime.now(); // 오늘 날짜
+  now = now.add(Duration(days: 7)); // 7일 추가
+  now = now.subtract(Duration(hours: 3)); // 3시간 전
+  print(now);
+
+  // 캐스케이드 방법
+  DateTime now2 = DateTime.now() // 오늘 날짜
+    ..add(Duration(days: 7)) // 7일 추가
+    ..subtract(Duration(hours: 3)); // 3시간 전
+  print(now2);
+}
+
+/// 4.주석
+void commentsFunction() {
+  // 한 줄 주석
+
+  /*
+   * 여러 줄 주석
+   */
+
+  /// 설명 주석
+
+  //? 코드 설명 주석
+  /// ```dart
+  /// DayOfWeek enumValue2 = DayOfWeek.friday
+  /// ```
+
+  //? 이미지 설명 주석
+  /// ![](https://flutter-ko.dev/images/catalog-widget-placeholder.png)
+}
+
+/// 5.메타데이터
+void metadataFunction() {
+  var manager = Manager();
+
+  // 1.override
+  manager.printMethod();
+
+  // 2.deprecated
+  manager.printMethod2();
+
+  // 3.custom
+  manager.printMethod3();
+}
+
+/// 6.라이브러리
+void libraryFunction(){
+  /*
+    import 'package:lib1/lib1.dart'; // 사용
+    import 'package:lib2/lib1.dart' as lib2; // lib2 식별자로 사용
+    import 'package:lib1/lib1.dart' show foo; // lib1에서 foo만 사용
+    import 'package:lib2/lib1.dart' hide foo; // lib1에서 foo만 제외
+  */
+}
+
+/// 7.키워드(https://dart.dev/language/keywords)
+void keywordsFunction() {
+
+  /// static
+  ///
+  /// 클래스 레벨에서만 사용 가능(클래스 멤버)
+
+  /*
+    class Queue {
+      static const initialCapacity = 16;
+    }
+  */
+
+  // late
+  //
+  // 선언 후 초기화되는 null을 허용하지 않는 변수를 선언합니다.
+  // 변수를 느리게 초기화합니다.
+
+  /*
+    late String description;
+  */
+
+  // typedef
+  //
+  // 데이터 타입을 새롭게 정의하는 키워드
+
+  /*
+    typedef IntList = List<int>;
+    IntList il = [1, 2, 3];
+  */
 }
 
 enum DayOfWeek {
@@ -105,7 +247,6 @@ enum DayOfWeek {
 
 /// dynamic과 object 차이
 void dynamicAndObject() {
-
   /// 대상은 어떤 것도 가정할 수 없으며 타입도 체크하지 말라는 뜻이다.
   /// 프로그래머가 어떤 타입인지 알고 있고 직접 처리한다고 가정하기 때문에 o.foo()와 같은 호출은 오히려 컴파일러가 경고하지 않는다.
   dynamic dynamicValue = 'flutter';
@@ -118,7 +259,6 @@ void dynamicAndObject() {
 
   // dynamicValue = null; // null 지정 가능
   // objectValue = null; //! null 지정 불가
-
 
   // 차이점2 --------------------------------------------
 
@@ -139,5 +279,31 @@ void dynamicAndObject() {
 }
 
 class Employee {
-  printEmp() => print("Employee Object");
+  void printMethod() {
+    print("Employee Object");
+  }
+}
+
+class Manager extends Employee {
+  @override
+  void printMethod() {
+    print("Manager Object");
+  }
+
+  @Deprecated('삭제 예정입니다.')
+  void printMethod2() {
+    print("Manager Object2");
+  }
+
+  @Todo('Dash', 'Implement this function')
+  void printMethod3() {
+    print("Manager Object2");
+  }
+}
+
+class Todo {
+  final String who;
+  final String what;
+
+  const Todo(this.who, this.what);
 }
